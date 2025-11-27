927d266f5300b9746da86869092ab3bfc0bad786
Arthur Araujo <arthurtute200@hotmail.com>
2025-11-27 17:01:32 -0300

    feat: implemented users module

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/user-schema';
+import { UsersController } from './user.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
