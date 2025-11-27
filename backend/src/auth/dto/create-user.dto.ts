db4e6d766211d227d9939726e963466ba57a999b
Arthur Araujo <arthurtute200@hotmail.com>
2025-11-26 21:02:28 -0300

    feat: created dtos for auth domain

---
+++ backend/src/auth/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  confirmPassword: string;
+}
