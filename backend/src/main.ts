ef646ed10c0d33976f25627b7312bf545f7a0f6d
Arthur Araujo <arthurtute200@hotmail.com>
2025-11-28 17:48:32 -0300

    feat: created the feature to register a standard user on the database as the api initializes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SeedService } from './seed.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const seedService = app.get(SeedService);
+  await seedService.createStandardUser();
+  await app.listen(process.env.PORT ?? 3002, () => {
+    console.log('Server running on port 3002');
+  });
 }
 bootstrap();
