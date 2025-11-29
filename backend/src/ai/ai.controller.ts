a83ea901c5d1a8ff380c92257070172b0d48e5d0
Arthur Araujo <arthurtute200@hotmail.com>
2025-11-29 16:59:17 -0300

    feat: implementing the logic on ai module with the ai/insights route

---
+++ backend/src/ai/ai.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AIService } from './ai.service';
+
+@Controller('ai')
+export class AIController {
+  constructor(private readonly aiService: AIService) {}
+
+  @Get('insights')
+  weatherInsight() {
+    return this.aiService.weatherInsight();
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/users.module';
 import { WeatherModule } from './weather/weather.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AIModule } from './ai/ai.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     UsersModule,
     WeatherModule,
+    AIModule,
   ],
 })
 export class AppModule {}
