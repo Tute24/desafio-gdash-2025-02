079f9c404e2792c73487f4d4cff6048d4113507f
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-01 20:45:41 -0300

    feat: implemented general zustand store

---
+++ frontend/src/stores/general/general.store.ts
@@ -0,0 +1,25 @@
+import type { GeneralStore } from '@/types/general-store-types'
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+export const useGeneralStore = create<GeneralStore>()(
+  persist(
+    (set) => ({
+      statusMessage: null,
+      aiInsights: null,
+      isLoading: false,
+      hasHydrated: false,
+
+      setStatusMessage: (statusMessage) => set({ statusMessage }),
+      setAiInsights: (aiInsights) => set({ aiInsights }),
+      setIsLoading: (isLoading) => set({ isLoading }),
+    }),
+    {
+      name: 'general-store',
+      storage: createJSONStorage(() => sessionStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) state.hasHydrated = true
+      },
+    },
+  ),
+)
