4360c225656d0a382655708a7f4acf6368eca2c2
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-03 20:50:50 -0300

    feat: created the dashboard main component

---
+++ frontend/src/components/dashboard-components/dashboard-main.tsx
@@ -0,0 +1,28 @@
+import { useWeatherStore } from '@/stores/weather/weather.store'
+import { CurrentWeatherCard } from './cards/current-weather-card'
+import { dateSlashParse } from '@/utils/date-slash-parse'
+import { FilesDownloader } from './files-downloader'
+import { DailyWeatherCard } from './cards/daily-weather-card'
+
+export default function DashboardMain() {
+  const current = useWeatherStore((store) => store.current)
+  const geo = useWeatherStore((store) => store.geo)
+  const daily = useWeatherStore((store) => store.daily)
+  return (
+    <>
+      <div className="my-5 ml-5 text-xs sm:text-sm font-poppins font-semibold">
+        <h2>
+          Data collected at:{' '}
+          <span className="text-cyan-700">
+            {dateSlashParse(current?.dt)}
+          </span>{' '}
+        </h2>
+      </div>
+      <div className="flex flex-col items-center text-center py-10 gap-10 px-2 sm:px-0">
+        <CurrentWeatherCard current={current!} geo={geo!} />
+        <DailyWeatherCard daily={daily} geo={geo!} />
+      </div>
+      <FilesDownloader />
+    </>
+  )
+}
