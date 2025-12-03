33580edac3e837e4299c9638bb5ddedae70abe76
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-02 21:16:40 -0300

    feat: implemented the sign out request

---
+++ frontend/src/api/auth/sign-out-request.ts
@@ -0,0 +1,35 @@
+import { AxiosApi } from '../axios-api'
+import RequestErrorHandler from '../request-error-handler'
+import { useAuthStore } from '@/stores/auth/auth.store'
+import { useUserStore } from '@/stores/user/user.store'
+import { useGeneralStore } from '@/stores/general/general.store'
+
+export async function signOutRequest() {
+  const { setToken } = useAuthStore.getState()
+  const { setId, setName, setEmail } = useUserStore.getState()
+  const { setIsLoading } = useGeneralStore.getState()
+  try {
+    setIsLoading(true)
+    const response = await AxiosApi({
+      httpMethod: 'post',
+      route: '/auth/sign-out',
+    })
+
+    if (response.status === 201) {
+      const responseData = response.data as { message: string }
+      setToken(null)
+      setId(null)
+      setName(null)
+      setEmail(null)
+      console.log(responseData.message)
+      return { success: true }
+    }
+
+    return { success: false }
+  } catch (error) {
+    RequestErrorHandler(error)
+    return { success: false }
+  } finally {
+    setIsLoading(false)
+  }
+}
