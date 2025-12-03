4e31b9ab088fee152547e3ec96a461cabe3ca5ef
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-03 20:51:24 -0300

    feat: implemented the getWeather call on the DashboardPage

--- frontend/src/pages/weather/dashboard/Dashboard.tsx
+++ frontend/src/pages/weather/dashboard/Dashboard.tsx
@@ -1,27 +1,14 @@
 import { getWeatherRequest } from '@/api/weather/get-weather-request'
-import { Button } from '@/components/ui/button'
-import { useWeatherStore } from '@/stores/weather/weather.store'
+import DashboardMain from '@/components/dashboard-components/dashboard-main'
 import { useEffect } from 'react'
 
 export default function DashboardPage() {
   useEffect(() => {
     getWeatherRequest()
   }, [])
-  const current = useWeatherStore((store) => store.current)
-  const hasHydrated = useWeatherStore((store) => store.hasHydrated)
-  if (!hasHydrated) {
-    return (
-      <>
-        <div>Hidratando...</div>
-      </>
-    )
-  }
-
   return (
     <>
-      <div>
-        Hello,World <Button>{current?.dt}</Button>
-      </div>
+      <DashboardMain />
     </>
   )
 }
