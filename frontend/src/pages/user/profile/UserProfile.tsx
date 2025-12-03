e40c78803ee11188c88f943ddce014ef99538c20
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-02 21:18:50 -0300

    feat: created the initial layout for the USerProfile andWeather Dashboard pages

---
+++ frontend/src/pages/user/profile/UserProfile.tsx
@@ -0,0 +1,9 @@
+import LoggedHeader from '@/components/headers/logged-header'
+
+export default function UserProfilePage() {
+  return (
+    <>
+      <LoggedHeader /> <div>Hello,World</div>
+    </>
+  )
+}
---
+++ frontend/src/pages/weather/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import LoggedHeader from '@/components/headers/logged-header'
+
+export default function DashboardPage() {
+  return (
+    <>
+      <LoggedHeader /> <div>Hello,World</div>
+    </>
+  )
+}
