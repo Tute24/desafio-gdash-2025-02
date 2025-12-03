4026c661c856c7fd519557d04390dc28cbedf8cd
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-03 20:48:04 -0300

    feat: created utils functions

---
+++ frontend/src/utils/to-title-case.ts
@@ -0,0 +1,8 @@
+export function toTitleCase(string: string | null) {
+  if (string)
+    return string
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+}
