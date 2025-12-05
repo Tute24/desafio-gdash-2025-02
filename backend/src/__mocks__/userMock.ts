a4cccdab1f96f55fc5c688876d36c774edd3f4b9
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-05 17:53:48 -0300

    test: implementing mocks

---
+++ backend/src/__mocks__/userMock.ts
@@ -0,0 +1,17 @@
+export const userMock = {
+  id: 'idMock',
+  name: 'User',
+  email: 'user@example.com',
+  role: 'admin',
+  password: 'Teste12345!',
+  token: 'jwt_token_123',
+  usersArray: [
+    {
+      name: 'User1',
+      email: 'user@example.com',
+      role: 'admin',
+      createdAt: '01-01-2025',
+      updatedAt: '01-02-2025',
+    },
+  ],
+};
