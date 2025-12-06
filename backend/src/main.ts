f62f3be10e5f39df956e5bcbdab3718fd7f0f4e0
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-06 14:47:30 -0300

    feat: implemented the front service on docker-compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { SeedService } from './seed.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
   });
   const seedService = app.get(SeedService);
